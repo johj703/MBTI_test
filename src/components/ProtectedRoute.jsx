14b836eb93208d6d991704eccd1be328d8b0a52d
Jo <johj703@nate.com>
2024-09-10 00:21:15 +0900

    add: props로 user와 children 추가하고 함수의 리턴 값은 children으로 변경

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,11 +1,11 @@
 import { Navigate } from "react-router-dom";
 
-const ProtectedRoute = () => {
+const ProtectedRoute = ({ user, children }) => {
   if (!user) {
     return <Navigate to="/login" />;
   }
 
-  return <div>ProtectedRoute</div>;
+  return children;
 };
 
 export default ProtectedRoute;

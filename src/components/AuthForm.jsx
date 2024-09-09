3b682c74bed135000fad8d3623b9fbeca6189b32
Jo <johj703@nate.com>
2024-09-09 23:45:11 +0900

    add: formData state 생성 및 input, button으로 로그인 및 회원가입 form 생성

--- src/components/AuthForm.jsx
+++ src/components/AuthForm.jsx
@@ -1,5 +1,30 @@
+import { useState } from "react";
+
 const AuthForm = () => {
-  return <div>AuthForm</div>;
+  const [formData, setFormData] = useState();
+
+  return (
+    <form>
+      <input
+        type="text"
+        name="id"
+        value={formData.id}
+        placeholder="아이디"
+        required
+      />
+      {mode === "signup" && (
+        <input
+          type="text"
+          name="nickname"
+          value={formData.nickname}
+          placeholder="닉네임"
+          required
+          className="w-full p-4 border border-gray-300 rounded-lg"
+        />
+      )}
+      <button type="submit">{mode === "login" ? "로그인" : "회원가입"}</button>
+    </form>
+  );
 };
 
 export default AuthForm;

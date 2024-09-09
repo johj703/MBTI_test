2bf41d5a2a533634847bf53f9c9943c397049982
Jo <johj703@nate.com>
2024-09-10 01:38:01 +0900

    fix: useEffect를 사용해서 로그인이 안 되어있을 때 페이지에 접근하려고 하면 login 페이지로 이동하도록 작성

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,31 +1,36 @@
-import { Link } from 'react-router-dom';
-const Layout = ({children}) => {
+import { useEffect } from "react";
+import { Link, Navigate, useNavigate } from "react-router-dom";
+
+const Layout = ({ children, user, setUser }) => {
+  const navigate = useNavigate();
+
+  // 사용자가 로그인하지 않은 상태에서 페이지에 접근하려고 할 때, login 페이지로 리디렉션
+  useEffect(() => {
+    if (!user) {
+      Navigate("/login");
+    }
+  }, [user, navigate]);
+
   return (
     <div>
-        <header>
-            <nav>
-                <Link to="/">
-                    홈
-                </Link>
-                <div>
-                    {user ? (
-                        <>
-                            {/* 다른 페이지로 가는 버튼도 필요 합니다 */}
-                            <button>
-                                로그아웃
-                            </button>
-                        </>
-                    ) : (
-                        <Link to="/login">
-                            로그인
-                        </Link>
-                    )}
-                </div>
-            </nav>
-        </header>
-        <main>{children}</main>
+      <header>
+        <nav>
+          <Link to="/">홈</Link>
+          <div>
+            {user ? (
+              <>
+                {/* 다른 페이지로 가는 버튼도 필요 합니다 */}
+                <button>로그아웃</button>
+              </>
+            ) : (
+              <Link to="/login">로그인</Link>
+            )}
+          </div>
+        </nav>
+      </header>
+      <main>{children}</main>
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;

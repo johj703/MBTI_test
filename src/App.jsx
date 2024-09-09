d36f4948052ec579997cfddd529b42a3c517f707
Jo <johj703@nate.com>
2024-09-10 01:02:27 +0900

    fix: 생성한 페이지들 Router 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,52 @@
 import { useState } from "react";
 import "./App.css";
+import { Route, Router, Routes } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import ProtectedRoute from "./components/ProtectedRoute";
+import ProfilePage from "./pages/ProfilePage";
+import TestResultPage from "./pages/TestResultPage";
+import TestPage from "./pages/TestPage";
 
 function App() {
   const [user, setUser] = useState(null);
-  return <>테스트</>;
+  return (
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login setUser={setUser} />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route
+            path="/profile"
+            element={
+              <ProtectedRoute user={user}>
+                <ProfilePage user={user} setUser={setUser} />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/test"
+            element={
+              <ProtectedRoute user={user}>
+                <TestPage user={user} />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/results"
+            element={
+              <ProtectedRoute user={user}>
+                <TestResultPage user={user} />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </Layout>
+    </Router>
+  );
 }
 
 export default App;

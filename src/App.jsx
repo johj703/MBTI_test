6e9d8348986baa122738e483d067944779aa89c9
Jo <johj703@nate.com>
2024-09-10 11:27:07 +0900

    fix: Router, Routes, Route 앞에 BrowserRouter as 추가해서 렌더링 되도록 작성

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./App.css";
-import { Route, Router, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import Login from "./pages/Login";

8d8873d549d79a7ea40e95bed274ef433d1b2f83
Jo <johj703@nate.com>
2024-11-29 00:54:00 +0900

    add: TestPage 작성

--- src/pages/TestPage.jsx
+++ src/pages/TestPage.jsx
@@ -1,5 +1,32 @@
-const TestPage = () => {
-  return <div>테스트 페이지 입니다.</div>;
+import React from "react";
+import TestForm from "../components/TestForm";
+import { calculateMBTI } from "../utils/mbtiCalculator";
+import { createTestResult } from "../api/testResults";
+import { useNavigate } from "react-router-dom";
+
+const Test = ({ user }) => {
+  const navigate = useNavigate();
+
+  const handleTestSubmit = async (answers) => {
+    const result = calculateMBTI(answers);
+    const resultData = {
+      userId: user.id,
+      nickname: user.nickname,
+      result,
+      answers,
+      date: new Date().toISOString(),
+      visibility: true,
+    };
+    await createTestResult(resultData);
+    navigate("/results");
+  };
+
+  return (
+    <div className="max-w-md mx-auto mt-10">
+      <h1 className="mb-4 text-2xl font-bold">MBTI 테스트</h1>
+      <TestForm onSubmit={handleTestSubmit} />
+    </div>
+  );
 };
 
-export default TestPage;
+export default Test;

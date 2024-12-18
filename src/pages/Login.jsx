c33af9199df0530d2aee09dcb84e3e12598cc5a5
Jo <johj703@nate.com>
2024-12-18 18:39:07 +0900

    fix: Login 페이지 JSX 작성

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -41,12 +41,38 @@ const Login = ({ setUser }) => {
     }
   };
   return (
-    <div>
+    <div className="">
       <h1>로그인 페이지 입니다.</h1>
-      {/* <AuthForm mode="login" onSubmit={handleLogin} /> */}
-      <div>
+      <form onSubmit={handleSubmit} className="">
+        <div>
+          <label className="">이메일: </label>
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            className=""
+            required
+          />
+        </div>
+        <div>
+          <label className="">비밀번호: </label>
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            className=""
+            required
+          />
+        </div>
+        <button type="submit" className="">
+          로그인
+        </button>
+      </form>
+      <div className="">
         <p>
-          계정이 없으신가요? <Link to="/signup">회원가입</Link>
+          계정이 없으신가요? <Link to="/signup" className="">회원가입</Link>
         </p>
       </div>
     </div>

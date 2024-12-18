93a7acad1fc2d6948afa36b658adb8c85b79601f
Jo <johj703@nate.com>
2024-12-18 18:24:15 +0900

    fix: handleSubmit 함수 작성

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,8 +2,23 @@ import { Link } from "react-router-dom";
 import Signup from "./Signup";
 
 const Login = () => {
-  const handleLogin = async (formData) => {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
     try {
+      // 실제 서버 연동 시에는 이 부분에서 서버로 로그인 요청을 보내야 함.
+      if(FormData.email && FormData.password){
+        const userData = {
+          email: FormData.email,
+          id: Date.now(), // 임시 사용자 ID
+        };
+
+        // 로컬 스토리지에 사용자 정보 저장
+        localStorage.setItem('user', JSON.stringify(userData));
+        // 전역 사용자 상태 업데이트
+        setUser(userData);
+        // 홈페이지로 리디렉션
+        Navigate('/');
+      }
     } catch (error) {
       alert("로그인에 실패 했습니다. 다시 시도해주세요.");
     }
@@ -14,7 +29,7 @@ const Login = () => {
       {/* <AuthForm mode="login" onSubmit={handleLogin} /> */}
       <div>
         <p>
-          계정이 없으신가요? <Link to="/Signup">회원가입</Link>
+          계정이 없으신가요? <Link to="/signup">회원가입</Link>
         </p>
       </div>
     </div>

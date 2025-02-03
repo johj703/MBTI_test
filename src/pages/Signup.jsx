72ce0a1b0c540c4b03bf3d1f8594ee12e5ca17a9
Jo <johj703@nate.com>
2025-02-03 19:02:19 +0900

    add: navigate 변수 할당 및 handleSignup 함수를 만들어서 AuthForm 컴포넌트에 onSubmit 함수로 등록

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,12 +1,22 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import AuthForm from "../components/AuthForm";
+import { register } from './../api/auth';
 
 const Signup = () => {
+  const navigate = useNavigate();
+
+  const handleSignup = async (formData) => {
+    try {
+      await register(formData);
+    } catch(error) {
+      alert("회원가입에 실패했습니다. 다시 시도해주세요.");
+    }
+  }
   return (
     <div>
       <div>
         <p>회원가입</p>
-        <AuthForm mode="signup" onSubmit={} />
+        <AuthForm mode="signup" onSubmit={handleSignup} />
         <div>
           <p>
             이미 계정이 있으신가요?{" "}

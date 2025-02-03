c0df05f1f943fa9bf19870dab9f0d6e4a73ca282
Jo <johj703@nate.com>
2025-02-03 17:27:37 +0900

    add: 로그인을 하지 않아도 접근할 수 있는 페이지를 배열로 추가

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,6 +4,9 @@ import { Link, useNavigate } from "react-router-dom";
 const Layout = ({ children, user, setUser }) => {
   const navigate = useNavigate();
 
+  // 로그인이 필요없는 페이지들 경로
+  const publicPages = ['/', '/login', 'signup'];
+
   useEffect(() => {
     // 현재 페이지가 public 페이지가 아니고, 사용자가 로그인하지 않은 경우에만 리다이렉트트
     if(!publicPages.includes(location.pathname) && !user) {

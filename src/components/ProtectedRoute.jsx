b15cf5f8daf9e103db1d453c70f8752f907255b4
Jo <johj703@nate.com>
2025-02-03 17:01:54 +0900

    add: 프로덕션 환경에서는 인증 체크 주석 추가

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,6 +1,8 @@
 import { Navigate } from "react-router-dom";
 
 export default function ProtectedRoute({ user, children }) {
+
+// 프로덕션 환경에서는 인증체크
   if (!user) {
     return <Navigate to="/login" />;
   }

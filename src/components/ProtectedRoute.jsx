bc954b624e71ed94f1c0568d925c0f9771e9ec4e
Jo <johj703@nate.com>
2025-02-03 17:03:05 +0900

    add: 개발 환경에서는 항상 접근 가능하도록 if문 추가

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,6 +1,10 @@
 import { Navigate } from "react-router-dom";
 
 export default function ProtectedRoute({ user, children }) {
+// 개발 환경에서는 항상 접근 허용
+if(process.env.NODE_ENV === 'development') {
+  return children
+}
 
 // 프로덕션 환경에서는 인증체크
   if (!user) {

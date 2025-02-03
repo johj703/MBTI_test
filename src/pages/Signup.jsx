4026654f854b6ca734361ca4c4ed1531614d09af
Jo <johj703@nate.com>
2025-02-03 18:46:34 +0900

    fix: 회원가입 페이지에 AuthForm 컴포넌트 추가 및 구성 요소 추가

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,5 +1,23 @@
+import { Link } from "react-router-dom";
+import AuthForm from "../components/AuthForm";
+
 const Signup = () => {
-  return <div>회원가입 페이지 입니다.</div>;
+  return (
+    <div>
+      <div>
+        <p>회원가입</p>
+        <AuthForm mode="signup" onSubmit={} />
+        <div>
+          <p>
+            이미 계정이 있으신가요?{" "}
+            <Link to="/login">
+              로그인
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  )
 };
 
 export default Signup;

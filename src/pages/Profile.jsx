4cee92fc54fea89b1d70f298a51009cccbb9a359
Jo <johj703@nate.com>
2025-06-02 16:45:06 +0900

    fix: 피드백 메세지 보여주는 부분 작성

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 const Profile = ({ user, setUser }) => {
   const [nickname, setNickname] = useState(user?.nickname || "");
+  const [message, setMessage] = useState("");
 
   const handleNicknameChange = (e) => {
     setNickname(e.target.value);
@@ -13,7 +14,17 @@ const Profile = ({ user, setUser }) => {
   return (
     <div>
       <div>
-        <h1>프로필 수정</h1>
+        <div>
+          <h1>프로필 수정</h1>
+          <p>개인 정보를 업데이트 하세요.</p>
+        </div>
+
+        {/* 피드백 메세지 */}
+        {message && (
+          <div>
+            <p>{message}</p>
+          </div>
+        )}
         <form onSubmit={handleSubmit}>
           <div>
             <label>닉네임</label>

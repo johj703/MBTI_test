43c2823a7abc392201a5373885968474156a258b
Jo <johj703@nate.com>
2025-06-02 18:39:25 +0900

    fix: Layout페이지에서 로그인 없이 프로필 수정 페이지로 이동할 수 있도록 proflie 경로 추가

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -8,7 +8,7 @@ const Layout = ({ children, user, setUser }) => {
   const location = useLocation();
 
   // 로그인이 필요없는 페이지들 경로
-  const publicPages = ["/", "/login", "/signup"];
+  const publicPages = ["/", "/login", "/signup", "/profile"];
 
   // 로그아웃 함수
   const handleLogout = () => {
@@ -19,7 +19,11 @@ const Layout = ({ children, user, setUser }) => {
 
   useEffect(() => {
     // 현재 페이지가 public 페이지가 아니고, 사용자가 로그인하지 않은 경우에만 리다이렉트
-    if (!publicPages.includes(location.pathname) && !user) {
+    if (
+      !publicPages.includes(location.pathname) &&
+      location.pathname !== "/profile" &&
+      !user
+    ) {
       navigate("/login");
     }
     // 의존성 배열: 이 값들이 변경될 때마다 effect 실행
